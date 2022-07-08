02c6051c726ea45b9bf7193cfc2af1b2976e14d6
TomasMillaKoch <TomasMK@cwf-fcf.org>
2022-07-08 15:52:20 -0400

    new marker structure

--- Tutorials/Jupyter_Book/mynewbook/assets/dashExtensions_default.js
+++ Tutorials/Jupyter_Book/mynewbook/assets/dashExtensions_default.js
@@ -1,16 +1,7 @@
 window.dashExtensions = Object.assign({}, window.dashExtensions, {
     default: {
         function0: function(feature, context) {
-            return context.props.hideout.includes(feature.name);
-        },
-        function1: function(feature, context) {
-            return context.props.hideout.includes(feature.name);
-        },
-        function2: function(feature, context) {
-            return context.props.hideout.includes(feature.name);
-        },
-        function3: function(feature, context) {
-            return context.props.hideout.includes(feature.name);
+            return L.circleMarker(feature.coordinates);
         }
     }
 });
